e291bcb1aca342f9c40c8dbcc5ff8b13690ac6e7
MaximPozdnyakow <maximpozdnyakow@gmail.com>
2020-04-19 20:52:22 +0300

    Create Schema for Products

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProductSchema = new Schema({
+    name: String,
+    category: String,
+    src: String,
+    price: Number
+});
+
+module.exports = Product = mongoose.model('product', ProductSchema);
+
