9f7a4b671170f715accd449e77e17af62ca8e3dc
MaximPozdnyakow <maximpozdnyakow@gmail.com>
2020-08-11 09:32:29 +0700

    fix gitignore

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: "./config/env/config.env" });
 
 const app = express();
 
