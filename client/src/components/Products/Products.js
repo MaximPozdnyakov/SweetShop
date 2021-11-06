6648e90936723f1adac5bc5c7e7ae1a3d9e0f6d0
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-06 17:31:43 +0300

    Connect Products component to mobx store, rewrite as class component

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,29 +1,31 @@
-import React, { useContext } from "react";
+import React from "react";
+import { observer, inject } from "mobx-react";
 
 import Card from "./Card";
-import { ProductsContext } from "../../context/Products/ProductsContext";
 
-function Products() {
-    const { products } = useContext(ProductsContext);
-
-    const cards = products.map((product) => (
-        <Card
-            id={product._id}
-            title={product.title}
-            category={product.category}
-            srcToImg={product.srcToImg}
-            price={product.price}
-            key={product._id}
-        />
-    ));
-
-    return (
-        <section className="text-gray-700 body-font">
-            <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-wrap -m-4">{cards}</div>
-            </div>
-        </section>
-    );
+@inject("ProductsStore")
+@observer
+class Products extends React.Component {
+    render() {
+        const { products } = this.props.ProductsStore;
+        const cards = products.map((product) => (
+            <Card
+                id={product._id}
+                title={product.title}
+                category={product.category}
+                srcToImg={product.srcToImg}
+                price={product.price}
+                key={product._id}
+            />
+        ));
+        return (
+            <section className="text-gray-700 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -m-4">{cards}</div>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Products;
