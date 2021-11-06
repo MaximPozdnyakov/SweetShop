860f8c59789856fb065361857ef1b860b9954783
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-06 13:46:40 +0300

    Create user mobx store, remove user context

--- client/src/stores/GlobalStore.js
+++ client/src/stores/GlobalStore.js
@@ -1,10 +1,12 @@
 import ProductsStore from "./ProductsStore";
 import MsgStore from "./MsgStore";
+import UserStore from "./UserStore";
 
 class GlobalStore {
     constructor() {
         this.ProductsStore = new ProductsStore(this);
         this.MsgStore = new MsgStore(this);
+        this.UserStore = new UserStore(this);
     }
 }
 
---
+++ client/src/stores/UserStore.js
@@ -0,0 +1,125 @@
+import { action, observable, when, makeObservable } from "mobx";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+class UserStore {
+    constructor(props) {
+        makeObservable(this);
+        this.MsgStore = props.MsgStore;
+        when(
+            () => !this.isUserLoaded,
+            () => this.fetchUser()
+        );
+    }
+
+    @observable user = {};
+    @observable isUserLoaded = false;
+    @observable isAuthenticated = false;
+
+    async fetchUser() {
+        try {
+            if (localStorage.getItem("token")) {
+                const user = await axios.get("/api/users", {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                });
+                if (!user.data.msg) {
+                    this.setUser(user.data.user);
+                    return;
+                }
+                localStorage.removeItem("token");
+            }
+            const user = await axios.get("/api/google-auth/login");
+            if (!user.data.msg) {
+                this.setUser(user.data.user);
+                return;
+            }
+            if (!localStorage.getItem("guest")) {
+                localStorage.setItem("guest", uuidv4());
+            }
+        } catch (e) {}
+    }
+
+    login = async (credentials) => {
+        try {
+            this.setUserNotLoaded();
+            const user = await axios.post("/api/users/login", credentials);
+            if (!user.data.msg) {
+                localStorage.setItem("token", user.data.token);
+                this.setUser(user.data.user);
+                return user;
+            }
+            this.MsgStore.setMsg(user.data.msg);
+            this.setUserLoaded();
+            return user.data;
+        } catch (e) {}
+    };
+
+    register = async (credentials) => {
+        try {
+            this.setUserNotLoaded();
+            const newUser = await axios.post("/api/users", credentials);
+            if (!newUser.data.msg) {
+                localStorage.setItem("token", newUser.data.token);
+                this.setUser(newUser.data.user);
+                return newUser;
+            }
+            this.MsgStore.setMsg(newUser.data.msg);
+            this.setUserLoaded();
+            return newUser.data;
+        } catch (e) {}
+    };
+
+    logout = async () => {
+        try {
+            this.setUserNotLoaded();
+            await axios.post(
+                "/api/users/logout",
+                {},
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                }
+            );
+            localStorage.removeItem("token");
+            this.setUserLogout();
+            this.setUserLoaded();
+        } catch (e) {}
+    };
+
+    googleLogout = async () => {
+        try {
+            this.setUserNotLoaded();
+            await axios.get("/api/google-auth/logout");
+            this.setUserLogout();
+            this.setUserLoaded();
+        } catch (e) {}
+    };
+
+    @action setUser(user) {
+        this.user = user;
+        this.isAuthenticated = true;
+        this.isUserLoaded = true;
+    }
+
+    @action setUserNotLoaded() {
+        this.isUserLoaded = false;
+    }
+
+    @action setUserLoaded() {
+        this.isUserLoaded = true;
+    }
+
+    @action setUserLogout() {
+        this.isAuthenticated = false;
+        this.user = {};
+    }
+}
+
+export default UserStore;
