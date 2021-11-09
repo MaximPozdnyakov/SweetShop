e3880283348c5b61554b13a84af65f2529250331
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-09 17:41:05 +0300

    Rewrite Cart component as class component, connect to mobx store

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,31 +1,33 @@
-import React, { useContext } from "react";
+import React from "react";
+import { observer, inject } from "mobx-react";
+import { If, Then, Else } from "react-if";
 
 import ListOfCartItems from "./ListOfCartItems";
 import Total from "./Total";
 
-import { CartContext } from "../../context/Cart/CartContext";
-
-import { If, Then, Else } from "react-if";
-
-function Cart() {
-    const { cartItems } = useContext(CartContext);
-    return (
-        <section className="text-gray-700 body-font">
-            <div className="container mx-auto flex mt-24 md:flex-row flex-col-reverse">
-                <If condition={cartItems.length === 0}>
-                    <Then>
-                        <div className="mx-auto text-2xl text-gray-900">
-                            Your cart is empty
-                        </div>
-                    </Then>
-                    <Else>
-                        <ListOfCartItems />
-                        <Total />
-                    </Else>
-                </If>
-            </div>
-        </section>
-    );
+@inject("CartStore")
+@observer
+class Cart extends React.Component {
+    render() {
+        const { cartItems } = this.props.CartStore;
+        return (
+            <section className="text-gray-700 body-font">
+                <div className="container mx-auto flex mt-24 md:flex-row flex-col-reverse">
+                    <If condition={cartItems.length === 0}>
+                        <Then>
+                            <div className="mx-auto text-2xl text-gray-900">
+                                Your cart is empty
+                            </div>
+                        </Then>
+                        <Else>
+                            <ListOfCartItems />
+                            <Total />
+                        </Else>
+                    </If>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Cart;
