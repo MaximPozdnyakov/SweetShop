558547c14bb2dc85f1720d192a3d342657a3feae
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-09 15:45:33 +0300

    Rewrite ProductPage as class component, connect to mobx store

--- client/src/components/Products/ProductPage.js
+++ client/src/components/Products/ProductPage.js
@@ -1,85 +1,90 @@
-import React, { useContext, useState, useEffect } from "react";
-import { ProductsContext } from "../../context/Products/ProductsContext";
-import { CartContext } from "../../context/Cart/CartContext";
-
+import React from "react";
+import { observer, inject } from "mobx-react";
+import { withRouter } from "react-router-dom";
 import { If, Then, Else } from "react-if";
 
-function ProductPage(props) {
-    const id = props.match.params.id;
+import Page404 from "../Page404";
 
-    const { products } = useContext(ProductsContext);
-    const { cartItems, addItem, deleteItemById } = useContext(CartContext);
-
-    const [product, setProduct] = useState(products.find((p) => p._id === id));
-    useEffect(() => {
-        setProduct(products.find((p) => p._id === id));
-    }, [products, id]);
-    const { title, price, srcToImg } = product;
-
-    const [cartItem, setCartItem] = useState(
-        cartItems.find((item) => item.productId === id)
-    );
-
-    useEffect(() => {
-        setCartItem(cartItems.find((item) => item.productId === id));
-    }, [cartItems, id]);
-
-    const addRemoveProduct = (e) => {
+@inject("ProductsStore", "CartStore")
+@observer
+class ProductPage extends React.Component {
+    toggleProduct = (e) => {
         e.preventDefault();
+        const { addCartItem, deleteCartItem } = this.props.CartStore;
+        const productId = this.props.match.params.id;
+        const cartItem = this.getCartItem();
         if (!cartItem) {
-            setCartItem({ id });
-            addItem({
-                productId: id,
-            });
+            addCartItem({ productId });
         } else {
-            setCartItem(undefined);
-            deleteItemById(cartItem._id);
+            deleteCartItem({ productId });
         }
     };
-    return (
-        <section className="text-gray-700 body-font overflow-hidden">
-            <div className="container px-5 py-24 mx-auto">
-                <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                    <img
-                        alt="ecommerce"
-                        className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                        src={srcToImg}
-                    />
-                    <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                        <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                            SWEET SHOP
-                        </h2>
-                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                            {title}
-                        </h1>
-                        <p className="leading-relaxed">
-                            Fam locavore kickstarter distillery. Mixtape
-                            chillwave tumeric sriracha taximy chia microdosing
-                            tilde DIY. XOXO fam indxgo juiceramps cornhole raw
-                            denim forage brooklyn. Everyday carry +1 seitan
-                            poutine tumeric. Gastropub blue bottle austin
-                            listicle pour-over, neutra jean shorts keytar banjo
-                            tattooed umami cardigan.
-                        </p>
-                        <div className="flex mt-4">
-                            <span className="title-font font-medium text-2xl text-gray-900">
-                                ${price}
-                            </span>
-                            <button
-                                className="flex ml-auto text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded"
-                                onClick={addRemoveProduct}
-                            >
-                                <If condition={!!cartItem}>
-                                    <Then>Remove product</Then>
-                                    <Else>Add product</Else>
-                                </If>
-                            </button>
+
+    getCartItem = () => {
+        const { cartItems } = this.props.CartStore;
+        const productId = this.props.match.params.id;
+        return cartItems.find((item) => item.productId === productId);
+    };
+
+    getProduct = () => {
+        const { products } = this.props.ProductsStore;
+        const productId = this.props.match.params.id;
+        return products.find((product) => product._id === productId);
+    };
+
+    render() {
+        const product = this.getProduct();
+        if (!product) {
+            return <Page404 />;
+        }
+        const { title, price, srcToImg } = product;
+        const cartItem = this.getCartItem();
+        return (
+            <section className="text-gray-700 body-font overflow-hidden">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+                        <img
+                            alt="ecommerce"
+                            className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                            src={srcToImg}
+                        />
+                        <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                            <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                                SWEET SHOP
+                            </h2>
+                            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                                {title}
+                            </h1>
+                            <p className="leading-relaxed">
+                                Fam locavore kickstarter distillery. Mixtape
+                                chillwave tumeric sriracha taximy chia
+                                microdosing tilde DIY. XOXO fam indxgo
+                                juiceramps cornhole raw denim forage brooklyn.
+                                Everyday carry +1 seitan poutine tumeric.
+                                Gastropub blue bottle austin listicle pour-over,
+                                neutra jean shorts keytar banjo tattooed umami
+                                cardigan.
+                            </p>
+                            <div className="flex mt-4">
+                                <span className="title-font font-medium text-2xl text-gray-900">
+                                    ${price}
+                                </span>
+                                <button
+                                    className="flex ml-auto text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded"
+                                    onClick={this.toggleProduct}
+                                >
+                                    <If condition={!!cartItem}>
+                                        <Then>Remove product</Then>
+                                        <Else>Add product</Else>
+                                    </If>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    );
+            </section>
+        );
+    }
 }
 
-export default ProductPage;
+export default withRouter(ProductPage);
