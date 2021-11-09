8c94b615a3e8de4aa755304cf2e40509afd6c220
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-09 17:44:41 +0300

    Rewrite ListOfCartItems component as class component, connect to mobx store, replace id prop with productId

--- client/src/components/Cart/ListOfCartItems.js
+++ client/src/components/Cart/ListOfCartItems.js
@@ -1,33 +1,36 @@
-import React, { useContext } from "react";
-import { v4 as uuid } from "uuid";
-
-import { CartContext } from "../../context/Cart/CartContext";
+import React from "react";
+import { observer, inject } from "mobx-react";
 
 import CartItem from "./CartItem";
-import { ProductsContext } from "../../context/Products/ProductsContext";
 
-export default function ListOfCardItems() {
-    const { cartItems } = useContext(CartContext);
-    const { products } = useContext(ProductsContext);
-
-    let cartItemsComponents = cartItems.map((item) => {
-        const product = products.find((p) => p._id === item.productId);
+@inject("ProductsStore", "CartStore")
+@observer
+class ListOfCardItems extends React.Component {
+    render() {
+        const { cartItems } = this.props.CartStore;
+        const { products } = this.props.ProductsStore;
+        const cartItemsComponents = cartItems.map((item) => {
+            const product = products.find(
+                (product) => product._id === item.productId
+            );
+            return (
+                <CartItem
+                    productId={item.productId}
+                    title={product.title}
+                    category={product.category}
+                    price={product.price}
+                    srcToImg={product.srcToImg}
+                    quantity={item.quantity}
+                    key={item.productId}
+                />
+            );
+        });
         return (
-            <CartItem
-                id={item._id}
-                title={product.title}
-                category={product.category}
-                price={product.price}
-                srcToImg={product.srcToImg}
-                quantity={item.quantity}
-                key={uuid()}
-            />
+            <div className="lg:w-3/4 md:w-2/3 w-full pr-md-8 pr-0 flex flex-row flex-wrap">
+                {cartItemsComponents}
+            </div>
         );
-    });
-
-    return (
-        <div className="lg:w-3/4 md:w-2/3 w-full pr-md-8 pr-0 flex flex-row flex-wrap">
-            {cartItemsComponents}
-        </div>
-    );
+    }
 }
+
+export default ListOfCardItems;
