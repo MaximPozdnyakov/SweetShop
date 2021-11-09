eff95788d3a657cb6f9e00fa6ab96af9b65b457e
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-09 17:42:14 +0300

    Rewrite CartTotal component as class component, connect to mobx store

--- client/src/components/Cart/Total.js
+++ client/src/components/Cart/Total.js
@@ -1,50 +1,51 @@
-import React, { useContext } from "react";
-
+import React from "react";
+import { observer, inject } from "mobx-react";
 import { Link } from "react-router-dom";
-
-import { CartContext } from "../../context/Cart/CartContext";
-import Stripe from "./Stripe";
-import { ProductsContext } from "../../context/Products/ProductsContext";
-import { UsersContext } from "../../context/Users/UsersContext";
-
 import { If, Then, Else } from "react-if";
 
-function Total() {
-    const { cartItems } = useContext(CartContext);
-    const { products } = useContext(ProductsContext);
-    const { isAuthenticated } = useContext(UsersContext);
+import Stripe from "./Stripe";
 
-    const total = cartItems.reduce(
-        (sum, item) =>
-            sum +
-            products.find((p) => p._id === item.productId).price *
-                item.quantity,
-        0
-    );
-
-    return (
-        <div className="lg:w-1/4 md:w-1/3 w-64 bg-gray-200 h-40 rounded-lg md:px-8 flex px-0 py-8 flex-col px-4 mx-auto mb-8">
-            <h2 className="text-gray-900 text-xl font-semibold title-font mb-5 text-center">
-                Total: ${total}
-            </h2>
-            <If condition={isAuthenticated}>
-                <Then>
-                    <Stripe price={total} />
-                </Then>
-                <Else>
-                    <div className="mx-auto text-md mt-2 text-gray-900">
-                        <Link
-                            to="/login"
-                            className="text-pink-500 underline hover:text-pink-600"
-                        >
-                            Login
-                        </Link>{" "}
-                        to make purchase
-                    </div>
-                </Else>
-            </If>
-        </div>
-    );
+@inject("ProductsStore", "UserStore", "CartStore")
+@observer
+class Total extends React.Component {
+    getTotal = () => {
+        const { cartItems } = this.props.CartStore;
+        const { products } = this.props.ProductsStore;
+        return cartItems.reduce(
+            (sum, item) =>
+                sum +
+                products.find((p) => p._id === item.productId).price *
+                    item.quantity,
+            0
+        );
+    };
+    render() {
+        const { isAuthenticated } = this.props.UserStore;
+        const total = this.getTotal();
+        return (
+            <div className="lg:w-1/4 md:w-1/3 w-64 bg-gray-200 h-40 rounded-lg md:px-8 flex px-0 py-8 flex-col px-4 mx-auto mb-8">
+                <h2 className="text-gray-900 text-xl font-semibold title-font mb-5 text-center">
+                    Total: ${total}
+                </h2>
+                <If condition={isAuthenticated}>
+                    <Then>
+                        <Stripe total={total} />
+                    </Then>
+                    <Else>
+                        <div className="mx-auto text-md mt-2 text-gray-900">
+                            <Link
+                                to="/login"
+                                className="text-pink-500 underline hover:text-pink-600"
+                            >
+                                Login
+                            </Link>{" "}
+                            to make purchase
+                        </div>
+                    </Else>
+                </If>
+            </div>
+        );
+    }
 }
 
 export default Total;
