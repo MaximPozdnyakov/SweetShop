c7b7d6ffe855ac3d47de0e3976d2d9642f8231b8
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-09 15:33:12 +0300

    Remove comments in ProductCard component, replace inner state with mobx Cart store

--- client/src/components/Products/Card.js
+++ client/src/components/Products/Card.js
@@ -6,36 +6,28 @@ import { If, Then, Else } from "react-if";
 @inject("CartStore")
 @observer
 class Card extends React.Component {
-    constructor(props) {
-        super(props);
-        const { cartItems } = this.props.CartStore;
-        this.state = {
-            cartItem: cartItems.find(
-                (item) => item.productId === this.props.id
-            ),
-        };
-    }
-
     toggleProduct = (e) => {
         e.preventDefault();
-        const { addCartItem, deleteItemById } = this.props.CartStore;
-        if (!this.state.cartItem) {
-            this.setState({
-                cartItem: { id: this.props.id },
-            });
-            addCartItem({
-                productId: this.props.id,
-            });
+        const { addCartItem, deleteCartItem } = this.props.CartStore;
+        const productId = this.props.id;
+        const cartItem = this.getCartItem();
+        if (!cartItem) {
+            addCartItem({ productId });
         } else {
-            this.setState({
-                cartItem: undefined,
-            });
-            deleteItemById(this.state.cartItem._id);
+            deleteCartItem({ productId });
         }
+    };
+
+    getCartItem = () => {
+        const { cartItems } = this.props.CartStore;
+        const productId = this.props.id;
+        return cartItems.find((item) => item.productId === productId);
     };
 
     render() {
         const { id, title, category, srcToImg, price } = this.props;
+        const cartItem = this.getCartItem();
+
         return (
             <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
                 <Link to={`/store/${id}`}>
@@ -58,7 +50,7 @@ class Card extends React.Component {
                             className="bg-pink-500 hover:bg-pink-600 px-3 py-1 absolute bottom-0 right-0 text-white rounded"
                             onClick={this.toggleProduct}
                         >
-                            <If condition={!!this.state.cartItem}>
+                            <If condition={!!cartItem}>
                                 <Then>
                                     <i className="fas fa-check"></i>
                                 </Then>
@@ -75,66 +67,3 @@ class Card extends React.Component {
 }
 
 export default Card;
-
-// function Card(props) {
-//     const { id, title, category, srcToImg, price } = props;
-
-//     const { cartItems, addItem, deleteItemById } = useContext(CartContext);
-
-//     const [cartItem, setCartItem] = useState(
-//         cartItems.find((item) => item.productId === id)
-//     );
-
-//     useEffect(() => {
-//         setCartItem(cartItems.find((item) => item.productId === id));
-//     }, [cartItems, id]);
-
-//     const addRemoveProduct = (e) => {
-//         e.preventDefault();
-//         if (!cartItem) {
-//             setCartItem({ id });
-//             addItem({
-//                 productId: id,
-//             });
-//         } else {
-//             setCartItem(undefined);
-//             deleteItemById(cartItem._id);
-//         }
-//     };
-
-//     return (
-//         <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-//             <Link to={`/store/${id}`}>
-//                 <div className="block relative h-48 rounded overflow-hidden">
-//                     <img
-//                         alt="ecommerce"
-//                         className="object-cover object-center w-full h-full block"
-//                         src={srcToImg}
-//                     />
-//                 </div>
-//                 <div className="mt-4 relative">
-//                     <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-//                         {category.toUpperCase()}
-//                     </h3>
-//                     <h2 className="text-gray-900 title-font text-lg font-medium">
-//                         {title}
-//                     </h2>
-//                     <p className="mt-1">${price}</p>
-//                     <button
-//                         className="bg-pink-500 hover:bg-pink-600 px-3 py-1 absolute bottom-0 right-0 text-white rounded"
-//                         onClick={addRemoveProduct}
-//                     >
-//                         <If condition={!!cartItem}>
-//                             <Then>
-//                                 <i className="fas fa-check"></i>
-//                             </Then>
-//                             <Else>
-//                                 <i className="fas fa-cart-plus"></i>
-//                             </Else>
-//                         </If>
-//                     </button>
-//                 </div>
-//             </Link>
-//         </div>
-//     );
-// }
