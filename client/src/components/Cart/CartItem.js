24c0058de5b96e9c10a314cbb6401f0503bd8567
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-09 17:52:36 +0300

    Rewrite CaertItem component as class component, connect to mobx store, refactor changeQuantity method

--- client/src/components/Cart/CartItem.js
+++ client/src/components/Cart/CartItem.js
@@ -1,88 +1,78 @@
-import React, { useContext, useState } from "react";
+import React from "react";
+import { observer, inject } from "mobx-react";
 
-import { CartContext } from "../../context/Cart/CartContext";
-
-import { If, Then } from "react-if";
-
-function CartItem(props) {
-    const { id, title, price, srcToImg, quantity, category } = props;
-
-    const { deleteItemById, updateQuantityOfItem } = useContext(CartContext);
-
-    const [quantityState, setQuantityState] = useState(quantity);
-
-    const handleDelete = (e) => {
+@inject("CartStore")
+@observer
+class CartItem extends React.Component {
+    deleteCartItem = (e) => {
         e.preventDefault();
-        setQuantityState(0);
-        deleteItemById(id);
+        const { productId } = this.props;
+        const { deleteCartItem } = this.props.CartStore;
+        deleteCartItem({ productId });
     };
 
-    const quantityPlus = (e) => {
+    changeQuantityBy = (e, difference) => {
         e.preventDefault();
-        setQuantityState(quantityState + 1);
-        updateQuantityOfItem(id, quantity + 1);
-    };
-    const quantityMinus = (e) => {
-        e.preventDefault();
-        setQuantityState(quantityState - 1);
-        if (quantity > 1) {
-            updateQuantityOfItem(id, quantity - 1);
+        const { quantity, productId } = this.props;
+        const { updateQuantityOfItem } = this.props.CartStore;
+        const newQuantity = quantity + difference;
+        if (newQuantity >= 1) {
+            updateQuantityOfItem({ productId, quantity: newQuantity });
         } else {
-            handleDelete();
+            this.deleteCartItem(e);
         }
     };
 
-    return (
-        <If condition={quantityState >= 1}>
-            <Then>
-                <div className="w-full lg:w-1/2 flex flex-wrap flex-row mb-8">
-                    <div className="w-1/2 h-48 overflow-hidden flex flex-row xl:justify-center justify-end">
-                        <img
-                            className="object-cover object-center h-full block"
-                            src={srcToImg}
-                            alt="..."
-                            style={{ minWidth: 200 }}
-                        />
+    render() {
+        const { title, price, srcToImg, quantity, category } = this.props;
+        return (
+            <div className="w-full lg:w-1/2 flex flex-wrap flex-row mb-8">
+                <div className="w-1/2 h-48 overflow-hidden flex flex-row xl:justify-center justify-end">
+                    <img
+                        className="object-cover object-center h-full block"
+                        src={srcToImg}
+                        alt="..."
+                        style={{ minWidth: 200 }}
+                    />
+                </div>
+                <div className="w-1/2 flex flex-col justify-between py-2 pl-4">
+                    <div>
+                        <div className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                            {category.toUpperCase()}
+                        </div>
+                        <div className="text-gray-900 title-font text-lg font-medium">
+                            {title}
+                        </div>
+                        <div className="text-gray-900 title-font text-lg font-semibold">
+                            ${price}
+                        </div>
                     </div>
-                    <div className="w-1/2 flex flex-col justify-between py-2 pl-4">
-                        <div>
-                            <div className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                {category.toUpperCase()}
-                            </div>
-                            <div className="text-gray-900 title-font text-lg font-medium">
-                                {title}
-                            </div>
-                            <div className="text-gray-900 title-font text-lg font-semibold">
-                                ${price}
-                            </div>
-                        </div>
-                        <div className="flex flex-row">
-                            Quantity:{" "}
-                            <div
-                                className="border rounded-full flex items-center justify-center w-5 h-5 ml-3 mr-2 cursor-pointer"
-                                onClick={quantityMinus}
-                            >
-                                <div className="h-3 mb-3">-</div>
-                            </div>{" "}
-                            {quantityState}{" "}
-                            <div
-                                className="border rounded-full flex items-center justify-center w-5 h-5 ml-2 cursor-pointer"
-                                onClick={quantityPlus}
-                            >
-                                <div className="h-3 mb-3">+</div>
-                            </div>
-                        </div>
+                    <div className="flex flex-row">
+                        Quantity:{" "}
                         <div
-                            className="inline-flex text-gray-700 bg-gray-200 w-24 border-0 py-1 pl-4 focus:outline-none hover:bg-gray-300 rounded text-lg cursor-pointer"
-                            onClick={handleDelete}
+                            className="border rounded-full flex items-center justify-center w-5 h-5 ml-3 mr-2 cursor-pointer"
+                            onClick={(e) => this.changeQuantityBy(e, -1)}
                         >
-                            Remove
+                            <div className="h-3 mb-3">-</div>
+                        </div>{" "}
+                        {quantity}{" "}
+                        <div
+                            className="border rounded-full flex items-center justify-center w-5 h-5 ml-2 cursor-pointer"
+                            onClick={(e) => this.changeQuantityBy(e, 1)}
+                        >
+                            <div className="h-3 mb-3">+</div>
                         </div>
+                    </div>
+                    <div
+                        className="inline-flex text-gray-700 bg-gray-200 w-24 border-0 py-1 pl-4 focus:outline-none hover:bg-gray-300 rounded text-lg cursor-pointer"
+                        onClick={this.deleteCartItem}
+                    >
+                        Remove
                     </div>
                 </div>
-            </Then>
-        </If>
-    );
+            </div>
+        );
+    }
 }
 
 export default CartItem;
