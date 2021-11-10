11751738a6c65d891ec3c43e44ea8dccff12cafd
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-10 15:27:46 +0300

    Rewrite App component as class component, connect to mobx store, remove store fetching

--- client/src/App.js
+++ client/src/App.js
@@ -1,79 +1,62 @@
-import React, { useEffect, useContext } from "react";
+import React from "react";
+import { observer, inject } from "mobx-react";
+import { Switch, Route, withRouter } from "react-router-dom";
 
-import Loader from "react-loader-spinner";
-
-import { Switch, Route, useLocation } from "react-router-dom";
-
-import { If, Then, Else } from "react-if";
-
+import Loader from "./components/Utils/Loader";
 import Navbar from "./components/Navbar/Navbar";
+
 import Home from "./components/Home";
-import RedirectToHome from "./components/ProtectedRoutes/RedirectToHome";
 import Products from "./components/Products/Products";
-import ProductPage from "./components/Products/ProductPage";
-import Login from "./components/Auth/Login";
-import Register from "./components/Auth/Register";
-
-import { UsersContext } from "./context/Users/UsersContext";
-import { ProductsContext } from "./context/Products/ProductsContext";
-import { CartContext } from "./context/Cart/CartContext";
-import ForwardRoute from "./components/ProtectedRoutes/ForwardRoute";
-import { MsgContext } from "./context/Msg/MsgContext";
-import Cart from "./components/Cart/Cart";
 import Page404 from "./components/Page404";
+import ProductPage from "./components/Products/ProductPage";
+import Cart from "./components/Cart/Cart";
+import RegisterPage from "./components/Register/RegisterPage";
+import LoginPage from "./components/Login/LoginPage";
 
-function App() {
-    const location = useLocation();
+import AuthRoute from "./components/Routes/AuthRoute";
+import HomeRoute from "./components/Routes/HomeRoute";
 
-    const { getUser, isUserLoaded } = useContext(UsersContext);
-    const { getProducts, isProductsLoaded } = useContext(ProductsContext);
-    const { getCartItems, isCartLoaded } = useContext(CartContext);
-    const { setMsg } = useContext(MsgContext);
-
-    useEffect(() => {
-        getProducts();
-        getUser();
-    }, []); // eslint-disable-line
-
-    useEffect(() => {
-        if (isUserLoaded) {
-            getCartItems();
+@inject("ProductsStore", "MsgStore", "UserStore", "CartStore")
+@observer
+class App extends React.Component {
+    componentDidUpdate(prevProps) {
+        const prevPath = prevProps.location.pathname;
+        const currentPath = this.props.location.pathname;
+        if (currentPath !== prevPath) {
+            this.props.MsgStore.setMsg("");
+            window.scrollTo(0, 0);
         }
-    }, [isUserLoaded]);  // eslint-disable-line
+    }
 
-    useEffect(() => {
-        setMsg("");
-        window.scrollTo(0, 0);
-    }, [location.pathname]);  // eslint-disable-line
+    render() {
+        const { isProductsLoaded } = this.props.ProductsStore;
+        const { isUserLoaded } = this.props.UserStore;
+        const { isCartLoaded } = this.props.CartStore;
 
-    return (
-        <If condition={isUserLoaded && isProductsLoaded && isCartLoaded}>
-            <Then>
+        if (!isProductsLoaded || !isUserLoaded || !isCartLoaded) {
+            return <Loader />;
+        }
+
+        return (
+            <>
                 <Navbar />
-
                 <Switch>
                     <Route exact path="/home" component={Home} />
                     <Route exact path="/store" component={Products} />
                     <Route exact path="/store/:id" component={ProductPage} />
                     <Route exact path="/cart" component={Cart} />
-                    <ForwardRoute exact path="/login" component={Login} />
-                    <ForwardRoute exact path="/register" component={Register} />
-                    <RedirectToHome exact path="/" />
+                    <AuthRoute exact path="/login" component={LoginPage} />
+                    <AuthRoute
+                        exact
+                        path="/register"
+                        component={RegisterPage}
+                    />
+                    <HomeRoute exact path="/" />
                     <Route component={Page404} />
                 </Switch>
-            </Then>
-            <Else>
-                <div className="absolute top-0 w-screen h-screen flex flex-wrap items-center justify-center">
-                    <Loader
-                        type="Oval"
-                        color="#ed64a6"
-                        height={80}
-                        width={80}
-                    />
-                </div>
-            </Else>
-        </If>
-    );
+            </>
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
