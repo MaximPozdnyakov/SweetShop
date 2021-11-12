e5e9dd24dfcb0e46bee4c67f4b26085c37d704ce
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-12 15:44:45 +0300

    Rewrite CartStore with typescript

--- client/src/stores/CartStore.ts
+++ client/src/stores/CartStore.ts
@@ -1,8 +1,27 @@
 import { makeAutoObservable, reaction, runInAction } from "mobx";
 import axios from "axios";
+import UserStore from "./UserStore";
 
-class CartStore {
-    constructor(props) {
+interface ICartItem {
+    __v?: number;
+    _id?: string;
+    productId: string;
+    ownerId: string;
+    quantity: number;
+}
+
+interface ICartStore {
+    cartItems: ICartItem[];
+    isCartLoaded: boolean;
+    UserStore: UserStore;
+}
+
+class CartStore implements ICartStore {
+    UserStore;
+    cartItems: ICartItem[] = [];
+    isCartLoaded = false;
+
+    constructor(props: { UserStore: UserStore }) {
         makeAutoObservable(this);
         this.UserStore = props.UserStore;
         reaction(
@@ -11,43 +30,36 @@ class CartStore {
         );
     }
 
-    cartItems = [];
-    isCartLoaded = false;
-
     fetchCart = async () => {
         if (!this.UserStore.isUserLoaded) return;
         try {
             this.setCartNotLoaded();
             const { userId } = this.UserStore;
-            const cartItems = await axios.post("/api/cart", {
-                userId,
-            });
-            this.setCartItems(cartItems.data);
+            const { data: cartItems }: { data: ICartItem[] } = await axios.post(
+                "/api/cart",
+                { userId }
+            );
+            this.setCartItems(cartItems);
         } catch (e) {}
     };
 
-    addCartItem = async ({ productId }) => {
+    addCartItem = async ({ productId }: { productId: string }) => {
         const { userId } = this.UserStore;
         runInAction(() => {
             this.cartItems = [
                 ...this.cartItems,
-                {
-                    productId,
-                    ownerId: userId,
-                    quantity: 1,
-                },
+                { productId, ownerId: userId, quantity: 1 },
             ];
         });
         try {
-            const newCartItem = await axios.post(`/api/cart/store`, {
-                productId,
-                quantity: 1,
-                userId,
-            });
+            const { data: newCartItem }: { data: ICartItem } = await axios.post(
+                `/api/cart/store`,
+                { productId, quantity: 1, userId }
+            );
             runInAction(() => {
                 this.cartItems = this.cartItems.map((item) => {
                     if (item.productId === productId) {
-                        item._id = newCartItem.data._id;
+                        item._id = newCartItem._id;
                     }
                     return item;
                 });
@@ -55,7 +67,13 @@ class CartStore {
         } catch (e) {}
     };
 
-    updateQuantityOfItem = async ({ productId, quantity }) => {
+    updateQuantityOfItem = async ({
+        productId,
+        quantity,
+    }: {
+        productId: string;
+        quantity: number;
+    }) => {
         runInAction(() => {
             this.cartItems = this.cartItems.map((item) => {
                 if (item.productId === productId) {
@@ -68,18 +86,17 @@ class CartStore {
             const { userId } = this.UserStore;
             const id = this.cartItems.find(
                 (item) => item.productId === productId
-            )._id;
-            await axios.put(`/api/cart/${id}`, {
-                quantity,
-                userId,
-            });
+            )!._id;
+            if (!id) return;
+            await axios.put(`/api/cart/${id}`, { quantity, userId });
         } catch (err) {}
     };
 
-    deleteCartItem = async ({ productId }) => {
+    deleteCartItem = async ({ productId }: { productId: string }) => {
         const id = this.cartItems.find(
             (item) => item.productId === productId
-        )._id;
+        )!._id;
+        if (!id) return;
         runInAction(() => {
             this.cartItems = this.cartItems.filter(
                 (item) => item.productId !== productId
@@ -87,41 +104,42 @@ class CartStore {
         });
         try {
             const { userId } = this.UserStore;
-            await axios.delete(`/api/cart/${id}`, {
-                data: { userId },
-            });
+            await axios.delete(`/api/cart/${id}`, { data: { userId } });
         } catch (e) {}
     };
 
     deleteAllCartItems = async () => {
-        runInAction(() => {
-            this.cartItems = [];
-        });
+        runInAction(() => (this.cartItems = []));
         try {
             const { userId } = this.UserStore;
-            await axios.delete(`/api/cart/owner`, {
-                data: { userId },
-            });
+            await axios.delete(`/api/cart/owner`, { data: { userId } });
         } catch (e) {}
     };
 
-    makePurchase = async ({ token, total }) => {
+    makePurchase = async ({
+        token,
+        total,
+    }: {
+        token: string;
+        total: number;
+    }) => {
         try {
             this.setCartNotLoaded();
-            const purchaseRes = await axios.post("/pay/charge", {
-                token,
-                product: {
-                    name: "Sweet Purchase",
-                    price: total * 100,
-                },
-                userToken: localStorage.getItem("token"),
-            });
+            type PurchaseResponse = { data: { status: string } };
+            const { data: purchaseRes }: PurchaseResponse = await axios.post(
+                "/pay/charge",
+                {
+                    token,
+                    product: { name: "Sweet Purchase", price: total * 100 },
+                    userToken: localStorage.getItem("token"),
+                }
+            );
             this.setCartLoaded();
-            return purchaseRes.data.status;
+            return purchaseRes.status;
         } catch (e) {}
     };
 
-    setCartItems(cartItems) {
+    setCartItems(cartItems: ICartItem[]) {
         this.cartItems = cartItems;
         this.isCartLoaded = true;
     }
